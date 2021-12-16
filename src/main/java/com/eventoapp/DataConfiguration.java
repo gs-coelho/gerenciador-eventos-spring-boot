7dfbf366d2b1704b406e67efd21dbd31b3a0be83
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2021-12-16 02:03:49 -0300

    Configurando a conexão com o banco de dados

---
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -0,0 +1,39 @@
+package com.eventoapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+
+        // Linha modificada para se adaptar melhor ao meu banco de dados
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDB103Dialect");
+        adapter.setPrepareConnection(true);
+
+        return adapter;
+    }
+}
