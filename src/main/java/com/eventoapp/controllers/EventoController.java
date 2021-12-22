f517d97cf1ed66a08673ffde1aef30631b31da72
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2021-12-22 19:34:36 -0300

    Buscando lista de Eventos
    Utiliza o EventoRepository para buscar uma lista de eventos, e adiciona eles à pagina usando o ModelAndView. Eles são exibidos no template utilizando as tags do Thymeleaf. Por enquanto, ainda não há nenhuma estilização.

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -23,4 +24,11 @@ public class EventoController {
         return "redirect:/cadastrarEvento";
     }
 
+    @RequestMapping(value="/eventos")
+    public ModelAndView listaEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventos = er.findAll();
+        mv.addObject("eventos", eventos);
+        return mv;
+    }
 }
