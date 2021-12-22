903a11ac82930df60d3ef2e5552ccf28b1a4ce29
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2021-12-22 19:24:24 -0300

    Criando o EventoRepository
    Esse commit cria o EventoRepository e o utiliza no EventoController para salvar um novo evento no banco de dados.

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,14 +1,26 @@
 package com.eventoapp.controllers;
 
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventoController {
 
-    @RequestMapping("/cadastrarEvento")
+    @Autowired
+    private EventoRepository er;
+
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
     public String form() {
         return "evento/formEvento";
+    }
+    @RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+    public String form(Evento evento) {
+        er.save(evento);
+        return "redirect:/cadastrarEvento";
     }
 
 }
